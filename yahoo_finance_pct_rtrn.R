64bcfdee6e16d9ca076e141994ec35d2ffc46b63
angelhenriquez1 <ahenriquez@scu.edu>
2020-08-27 09:13:40 -0700

    Yahoo finance (created loop for a specific estimated return)

--- yahoo_finance_pct_rtrn.R
+++ yahoo_finance_pct_rtrn.R
@@ -1,7 +1,27 @@
-# yahoo finance, percent return
 
+# finding list of all stock symbols
+other <- read_delim("http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt",
+                          "|", escape_double = FALSE, trim_ws = TRUE)
+
+nasdaq <- read_delim("http://ftp.nasdaqtrader.com/dynamic/SymDir/nasdaqtraded.txt",
+                          "|", escape_double = FALSE, trim_ws = TRUE)
+
+stock_names1 <- other[1]
+names(stock_names1)[1] <- "stock_symbols"
+stock_names2 <- nasdaq[1]
+names(stock_names2)[1] <- "stock_symbols"
+
+stock_names <- rbind(stock_names1, stock_names2)
+rm(other)
+rm(nasdaq)
+rm(stock_names1)
+rm(stock_names2)
+
+# yahoo finance, percent return
 yahoo_finance <- function(stock_sign){
   
+  stock_sign <- as.character(stock_sign)
+  stock_sign <- tolower(stock_sign)
   yahoo_url <- paste0("https://finance.yahoo.com/quote/", stock_sign, "?p=", stock_sign)
   
   url <- read_html(yahoo_url)
@@ -26,39 +46,63 @@ yahoo_finance <- function(stock_sign){
   value_ <-  sub(" i.*", "", est_return)    
   perc_return <-  sub(".*i ", "", est_return)    
   estimates <- paste0(value_, ' (', perc_return, ')')
-  
-  print("Yahoo Finance")
-  print(estimates)
+  print(stock_sign)
+  ifelse(perc_return > 25, estimates, "")
+  #print(estimates)
   
 }
 
-data <- c("GOOG")
+yahoo_finance("baba")
 
-yahoo_finance(data)
+data1 <- as.character(stock_names$stock_symbols)
 
+for ( i in data1 ){
 
+  yahoo_finance(i)
+  print(" ")
 
+}
 
-stock_sign = "aapl"
-yahoo_url <- paste0("https://finance.yahoo.com/quote/", stock_sign, "?p=", stock_sign)
+# use transmute instead of subsets
+a <- transmute(data)
 
-url <- read_html(yahoo_url)
-words <- url %>%
-  html_nodes(".IbBox") %>%
-  html_text() %>%
-  as.data.frame()
+pick_perc_return <- function(stock_sign){
+  
+  stock_sign <- as.character(stock_sign)
+  stock_sign <- tolower(stock_sign)
+  yahoo_url <- paste0("https://finance.yahoo.com/quote/", stock_sign, "?p=", stock_sign)
 
-est_return <- words[20,1]
-est_return <- as.character(est_return)
+  url <- read_html(yahoo_url)
+  words <- url %>%
+    html_nodes(".IbBox") %>%
+    html_text() %>%
+    as.data.frame()
 
-# removing left side
-est_return <- gsub(".*XX", "\\1", est_return)
-# removing right side
-est_return <- gsub("Premium.*", "\\1", est_return)
+  est_return <- words[20,1]
+  est_return <- as.character(est_return)
 
-print("Yahoo Finance")
-print(est_return)
+  # removing left side
+  est_return <- gsub(".*XX", "\\1", est_return)
+  # removing right side
+  est_return <- gsub("Premium.*", "\\1", est_return)
 
+  est_return1 <- regmatches(est_return, gregexpr("[[:digit:]]+", est_return))
+  est_return1 <- as.numeric(est_return1)
+
+  #print(stock_sign)
+  ifelse(est_return1 > 25, paste0(stock_sign, ": ", est_return1,"%"), "")
+
+}
+pick_perc_return("tsla")
+
+data1 <- as.character(stock_names$stock_symbols)
+
+for ( i in data1 ){
+  
+  more_than_25_percent_return(i)
+  print(" ")
+  
+}
 
 
 
